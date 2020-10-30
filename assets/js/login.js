ecb6890ffc791ca25677769dccb9f00edc8487f3
Kerwin <1078933746@qq.com>
2020-10-30 15:02:16 +0800

    用正则来验证输入的合法性

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,18 @@
-$('#login-box').on('click',function() {
+$('#login-box').on('click', function () {
     $('.login-box').toggle()
     $('.reg-box').toggle()
 })
 
-$('#reg-box').on('click',function() {
+$('#reg-box').on('click', function () {
     $('.login-box').toggle()
     $('.reg-box').toggle()
+})
+
+let form = layui.form;
+form.verify({
+    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    repwd: function (value) {
+        let pwd = $('.reg-box [name=password]').val();
+        if (value !== pwd) return '两次密码不一致'
+    }
 })
