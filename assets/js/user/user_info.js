5df3af37f76c3039ce58eace35c1822b1aa59623
Kerwin <1078933746@qq.com>
2020-11-01 16:11:35 +0800

    完成修改用户信息

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -1,5 +1,6 @@
 $(function() {
-    var form = layui.form
+    let form = layui.form
+    let layer = layui.layer
     form.verify({
       nickname: function(value) {
         if (value.length > 6) {
@@ -7,4 +8,43 @@ $(function() {
         }
       }
     })
+
+    initUserInfo()
+
+    function initUserInfo() {
+        $.ajax({
+            type: 'GET',
+            url: '/my/userinfo',
+            success(res) {
+                if (res.status !== 0){ 
+                    return layer.msg('获取用户信息失败')}
+                    console.log(res.data);
+                form.val('formUserInfo',res.data)
+  
+            }
+        })
+    }
+
+    $('#btnReset').on('click',function(e) {
+        e.preventDefault()
+        initUserInfo()
+    })
+    
+    $('.layui-form').on('submit',function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success(res) {
+                if(res.status !== 0){
+                    return layer.msg('更新用户信息失败')
+                }
+                layer.msg('更新用户信息成功')
+                window.parent.getUserInfo()
+            }
+        })
+    })
   })
+
+ 
