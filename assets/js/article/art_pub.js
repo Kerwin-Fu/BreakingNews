da9a57c0d9cf55e6dd7838eb5602291b4d6e2207
Kerwin <1078933746@qq.com>
2020-11-04 16:27:15 +0800

    完成创建文章

--- assets/js/article/art_pub.js
+++ assets/js/article/art_pub.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     initCate()
 
     function initCate() {
@@ -10,9 +10,90 @@ $(function() {
                 if (res.status !== 0) {
                     layui.layer.msg('获取文章分类列表失败')
                 }
-                let htmlStr = template('tpl-cate',res);
+                let htmlStr = template('tpl-cate', res);
                 $('[name=cate_id]').html(htmlStr);
                 layui.form.render()
+            }
+        })
+    }
+
+    // 初始化富文本编辑器
+    initEditor()
+
+    // 1. 初始化图片裁剪器
+    let $image = $('#image')
+
+    // 2. 裁剪选项
+    let options = {
+        aspectRatio: 400 / 280,
+        preview: '.img-preview'
+    }
+
+    // 3. 初始化裁剪区域
+    $image.cropper(options)
+
+    $('#btnChooseImage').on('click', function () {
+        $('#coverFile').click();
+    })
+
+    // 监听 coverFile 的 change 事件，获取用户选择的文件列表
+    $('#coverFile').on('change', function (e) {
+        // 获取到文件的列表数组
+        let files = e.target.files
+        // 判断用户是否选择了文件
+        if (files.length === 0) {
+            return
+        }
+        // 根据文件，创建对应的 URL 地址
+        let newImgURL = URL.createObjectURL(files[0])
+        // 为裁剪区域重新设置图片
+        $image
+            .cropper('destroy') // 销毁旧的裁剪区域
+            .attr('src', newImgURL) // 重新设置图片路径
+            .cropper(options) // 重新初始化裁剪区域
+    })
+
+
+    let art_state = '已发布';
+
+    $('#btnSave2').on('click', function () {
+        art_state = '草稿'
+    })
+
+    $('#form-pub').on('submit', function (e) {
+        e.preventDefault()
+        let fd = new FormData(this)
+        fd.append('state', art_state)
+        // 4. 将封面裁剪过后的图片，输出为一个文件对象
+        $image
+            .cropper('getCroppedCanvas', {
+                // 创建一个 Canvas 画布
+                width: 400,
+                height: 280
+            })
+            .toBlob(function (blob) {
+                // 将 Canvas 画布上的内容，转化为文件对象
+                // 得到文件对象后，进行后续的操作
+                // 5. 将文件对象，存储到 fd 中
+                fd.append('cover_img', blob)
+                // 6. 发起 ajax 数据请求
+                publishArticle(fd)
+            })
+
+    })
+
+    function publishArticle(fd) {
+        $.ajax({
+            type: 'POST',
+            url: '/my/article/add',
+            data: fd,
+            contentType: false,
+            processData: false,
+            success(res) {
+                if (res.status !== 0) return layui.layer.msg('发布文章失败')
+                layui.layer.msg('发布文章成功')
+                location.href = '/article/art_list.html'
+                window.parent.document.querySelector('[href="/article/art_list.html"]').click();
             }
         })
     }
